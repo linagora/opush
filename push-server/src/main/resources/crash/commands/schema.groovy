9392188a2fca4a97a90a551a1dadd81dc0823216
Thomas Hilaire <thilaire@linagora.com>
2014-03-25 16:16:05 +0100

    OBMFULL-5834 Use groovy strings

--- push-server/src/main/resources/crash/commands/schema.groovy
+++ push-server/src/main/resources/crash/commands/schema.groovy
@@ -1,3 +1,34 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Copyright (C) 2014 Linagora
+ *
+ * This program is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU Affero General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version, provided you comply 
+ * with the Additional Terms applicable for OBM connector by Linagora 
+ * pursuant to Section 7 of the GNU Affero General Public License, 
+ * subsections (b), (c), and (e), pursuant to which you must notably (i) retain 
+ * the “Message sent thanks to OBM, Free Communication by Linagora” 
+ * signature notice appended to any and all outbound messages 
+ * (notably e-mail and meeting requests), (ii) retain all hypertext links between 
+ * OBM and obm.org, as well as between Linagora and linagora.com, and (iii) refrain 
+ * from infringing Linagora intellectual property rights over its trademarks 
+ * and commercial brands. Other Additional Terms apply, 
+ * see <http://www.linagora.com/licenses/> for more details. 
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License 
+ * for more details. 
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * and its applicable Additional Terms for OBM along with this program. If not, 
+ * see <http://www.gnu.org/licenses/> for the GNU Affero General Public License version 3 
+ * and <http://www.linagora.com/licenses/> for the Additional Terms applicable to 
+ * OBM connectors. 
+ * 
+ * ***** END LICENSE BLOCK ***** */
 import org.crsh.text.ui.UIBuilder
 import org.obm.push.cassandra.schema.StatusSummary.Status
 import org.obm.push.cassandra.schema.CQLScriptExecutionStatus
@@ -16,27 +47,28 @@ class schema extends CRaSHCommand {
     switch (statusSummary.getStatus()) {
     
       case Status.UP_TO_DATE:
-         out << "OK\n"
-         out << "Your schema is already at the latest version: ${inline(statusSummary.getCurrentVersion())}"
+         out << """OK
+               |Your schema is already at the latest version: ${inline(statusSummary.getCurrentVersion())}""".stripMargin()
          break
          
       case Status.UPGRADE_AVAILABLE:
-         out << "WARN: Update advised\n"
-         out << "This opush server IS compatible with the current schema but an update is available \n"
-         out << "Current: ${inline(statusSummary.getCurrentVersion())}\n"
-         out << "Latest : ${statusSummary.getUpgradeAvailable().get()}"
+         out << """WARN: Update advised
+               |This opush server IS compatible with the current schema but an update is available
+               |Current: ${inline(statusSummary.getCurrentVersion())}
+               |Latest : ${statusSummary.getUpgradeAvailable().get()}""".stripMargin()
          break
          
       case Status.UPGRADE_REQUIRED:
-         out << "WARN: Update required\n"
-         out << "This opush server IS NOT compatible with the current schema, please update it \n" 
-         out << "Current: ${inline(statusSummary.getCurrentVersion())}\n"
-         out << "Latest : ${statusSummary.getUpgradeAvailable().get()}"
+         out << """WARN: Update required
+               |This opush server IS NOT compatible with the current schema, please update it
+               |Current: ${inline(statusSummary.getCurrentVersion())}
+               |Latest : ${statusSummary.getUpgradeAvailable().get()}""".stripMargin()
          break
          
       case Status.NOT_INITIALIZED:
-         out << "No schema found, you can create it using the \"install\" command\n"
-         out << "The latest schema version available is ${statusSummary.getUpgradeAvailable().get()}"
+         out << """WARN: Install required
+               |No schema found, you can create it using the "install" command
+               |The latest schema version available is ${statusSummary.getUpgradeAvailable().get()}""".stripMargin()
          break
          
       default:
@@ -64,7 +96,7 @@ class schema extends CRaSHCommand {
         out << "An error occurred when installing the schema: ${e.getMessage()}"
       }
     } else {
-      out << "The schema is already installed, use the \"status\" command to find if an update is available"
+      out << """The schema is already installed, use the "status" command to find if an update is available""".stripMargin()
     }
   }
 
