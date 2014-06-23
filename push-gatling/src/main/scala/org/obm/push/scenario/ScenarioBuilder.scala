939f6a1cbcada432f89e3e9f481eaba44108748e
Thomas Hilaire <thilaire@linagora.com>
2014-06-23 11:13:35 +0200

    OP-59 Remove all gatling stuff, see OBMLOAD

--- push-gatling/src/main/scala/org/obm/push/scenario/ScenarioBuilder.scala
+++
@@ -1,9 +0,0 @@
-package org.obm.push.scenario
-
-import org.obm.push.context.Configuration
-
-trait ScenarioBuilder {
-
-	def build(configuration: Configuration): io.gatling.core.structure.ScenarioBuilder
-	
-}
